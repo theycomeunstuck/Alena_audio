c9903e77d3cd3daae4c9037ca845ef5a751fa3eb
name <defoltwr@gmail.com>
2025-07-02 14:24:42 +0700

    Реализовано подавление шума. dns 16k

--- audio_enhancement.py
+++ audio_enhancement.py
@@ -1,43 +1,43 @@
 # audio_enhancement.py
 import torch, torchaudio
 import numpy as np
-from speechbrain.inference.separation import SepformerSeparation as separator
-from config import device
+import soundfile as sf
+# from speechbrain.inference.VAD import VAD
+from config import device, noise_Model
 from audio_utils import normalize_rms
 
-# 1. Загружаем предобученную модель
-model = separator.from_hparams(
-    source="speechbrain/sepformer-dns4-16k-enhancement",
-    savedir="pretrained_models/sepformer-dns4-16k-enhancement",
-    run_opts={"device":device}
-)
-
-# 2. Разделяем аудиофайл на источники (здесь — «усиленная» речь и «остаточный шум»)
-# est_sources = model.separate_file(
-#     path="speechbrain/sepformer-dns4-16k-enhancement/example_dns4-16k.wav"
-# )
 
 
-def noise_suppresion_SB(signal: np.ndarray) -> np.ndarray:
+
+count = 0
+
+def noise_suppresion_SB(audio: np.ndarray) -> np.ndarray:
     """
     Улучшение речи с помощью SepFormer.
     signal: 1D np.float32-массив, диапазон [-1, 1], fs=16 000 З
     возвращает: улучшенный сигнал того же формата.
     """
+    try:
+        # Переводим в тензор (batch=1)
+        Wav = torch.from_numpy(audio).unsqueeze(0).to(device)  # (1, N)
 
-    # Переводим в тензор (batch=1)
-    wav = torch.from_numpy(signal).unsqueeze(0).to(device)  # (1, N)
+        # 2) Отключаем градиенты, чтобы не тратить память и не считать бэкап для backward
+        with torch.no_grad():
+            # 3) SepFormer делает всю работу на GPU, потому что и модель, и вход на cuda
+            # Разделяем на источники: [speech, noise]
+            est_sources = noise_Model.separate_batch(Wav)  # (#batch, source, samples) на устройстве device
 
-    # 2) Отключаем градиенты, чтобы не тратить память и не считать бэкап для backward
-    with torch.no_grad():
-        # 3) SepFormer делает всю работу на GPU, потому что и модель, и вход на cuda
-        # Разделяем на источники: [speech, noise]
-        est_sources = model.separate_batch(wav)  # (#batch, source, samples) на устройстве device
+        # Берём первый источник (речь)
+        enhanced = est_sources[:, :].detach().cpu().squeeze()
 
-    # Берём первый источник (речь)
-    enhanced = est_sources[0, 0, :].detach().cpu().numpy()
+        # 4) Подгоняем RMS-уровень
+        enhanced = normalize_rms(enhanced, target_dBFS=-25)
+        global count
+        sf.write(f'wav/enhanced_torch{count}.wav', enhanced, 16000)
+        # torchaudio.save(f'wav/enhanced_torch{count}.wav', enhanced, 16000, encoding='PCM_S', bits_per_sample=16)
+        count += 1
+        return enhanced
+    except Exception as e:
+        print(f"[ERROR] Ошибка вызвана в файле {__file__} \n\n{e}")
+        raise Exception
 
-    # 4) Подгоняем RMS-уровень
-    enhanced = normalize_rms(enhanced)
-
-    return enhanced
