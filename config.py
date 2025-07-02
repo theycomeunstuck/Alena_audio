a0db877aa01968ccdd25216271cc222807782699
name <defoltwr@gmail.com>
2025-07-02 14:27:29 +0700

    Инициализация noise_model и whisper turbo, если cuda cores available

--- config.py
+++ config.py
@@ -2,25 +2,36 @@
 import warnings
 import whisper
 from torch.cuda import is_available
+from speechbrain.inference.separation import SepformerSeparation as separator
+
 # ——— ПАРАМЕТРЫ —————————————————————————————————————————————————————
 SAMPLE_RATE         = 16000
 NOISE_DURATION      = 2        # сек для профиля шума
-VAD_AGGR_MODE       = 3        # от 0 (мягко) до 3 (агрессивно) (voice activity detection)
+VAD_AGGR_MODE       = 1       # от 0 (мягко) до 3 (агрессивно) (voice activity detection)
 FRAME_MS            = 30       # размер VAD-фрейма в мс
-SPK_WINDOW_S        = 2        # размер окна для верификации, сек
+SPK_WINDOW_S        = 3        # размер окна для верификации, сек
 STEP_S              = 1       # шаг сдвига окна, сек.  должно быть целым числом
-MIN_VOICE_RATIO     = 0.6         # минимальная доля реальные речи в окне для ASR. # Параметры гейтинга ASR:
-MAX_ASR_FAILURES    = 3          # необязательный: макс. подряд «фоновых» окон до сброса. # Параметры гейтинга ASR:
+MIN_VOICE_RATIO     = 0.5         # минимальная доля реальные речи в окне для ASR. # Параметры гейтинга ASR:
+MAX_ASR_FAILURES    = 4          # необязательный: макс. подряд «фоновых» окон до сброса. # Параметры гейтинга ASR:
 TARGET_DBFS         = -18.0       # dBFS для RMS-нормализации
-WHISPER_MODEL       = "small"      # tiny, base, small, medium, turbo, large
 REFERENCE_FILE      = "reference.npy"
 REFERENCE_FILE_WAV  = "reference.wav"
 
+WHISPER_MODEL       = "small"      # tiny, base, small, medium, turbo, large
 device = "cuda" if is_available() else "cpu"
+
+if device == "cuda":
+    WHISPER_MODEL = "turbo"  # tiny, base, small, medium, turbo, large
+
 # ————————————————————————————————————————————————————————————————————
 
 # Загрузка модели ASR
 asr_model = whisper.load_model(WHISPER_MODEL, device=device)
+noise_Model = separator.from_hparams(
+    source="speechbrain/sepformer-dns4-16k-enhancement",
+    savedir="pretrained_models/sepformer-dns4-16k-enhancement",
+    run_opts={"device":device}).eval()
+
 
 # Отключаем предупреждения Whisper и PyTorch
 warnings.filterwarnings(
