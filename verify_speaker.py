13ff624d5f25f2f60c97769eac74a4bb4dbc9585
name <defoltwr@gmail.com>
2025-07-02 17:22:30 +0700

    выяснил с какого момента портится аудио

--- verify_speaker.py
+++ verify_speaker.py
@@ -30,23 +30,23 @@ def verify_speaker():
     step_samples = STEP_S * SAMPLE_RATE
     encoder = VoiceEncoder(device=device)
     asr_fail = 0
-
+    counter = 0 #todo:
     print("[VERIFY] Стриминг, Ctrl+C чтобы выйти")
-    with sd.RawInputStream(samplerate=SAMPLE_RATE, blocksize=step_samples,
-                           dtype=np.int16, channels=1) as stream:
+    with sd.InputStream(samplerate=SAMPLE_RATE, blocksize=step_samples,
+                           dtype=np.float32, channels=1) as stream:
         try:
             while True:
                 data, _ = stream.read(step_samples)
-                raw = bytes(data)
 
                 # VAD
-                voiced, total = bytearray(), 0
-                for i in range(0, len(raw), frame_size):
-                    frame = raw[i:i+frame_size]
-                    if len(frame)<frame_size: break
+                voiced, total = [], 0
+                int16_frame = (data * 32767).astype(np.int16)
+                for i in range(0, len(int16_frame), frame_size):
+                    frame_i16  = int16_frame[i:i+frame_size]
+                    if len(frame_i16 )<frame_size: break
                     total += 1
-                    if vad.is_speech(frame, SAMPLE_RATE):
-                        voiced.extend(frame)
+                    if vad.is_speech(frame_i16 , SAMPLE_RATE):
+                        voiced.append(data[i:i+frame_size].squeeze()) # original fl32
 
                 if total == 0 or (len(voiced)/total) < MIN_VOICE_RATIO:
                     asr_fail += 1
@@ -57,15 +57,23 @@ def verify_speaker():
                 asr_fail = 0
 
                 # очистка и накопление
-                audio = np.frombuffer(bytes(voiced), dtype=np.int16).astype(np.float32)/32768.0
+                # audio = np.frombuffer(bytes(voiced), dtype=np.int16).astype(np.float32)/32768.0
+                audio = np.concatenate(voiced)
                 spk_buf = np.concatenate([spk_buf, audio])[-win_samples:]
 
                 # верификация
                 if len(spk_buf) >= win_samples:
-
+                    #todo: сейчас опция 1
                     # todo: Проверить как будет работать, если: 1) очистка, speaker; 2) speaker; очистка    ?
-                    clean_audio = noise_suppresion_SB(spk_buf) # Сейчас реализован 1 вариант
+                    import soundfile as sf
+                    sf.write(f"debug_wav/fully_naked_{counter}.wav", spk_buf, SAMPLE_RATE)
+                    counter += 1
 
+                    # TODO: ДО СЮДА С АУДИО ВСЁ НОРМ
+                    #  КОПАЙ НА ШУМЫ, ВИДИМО
+
+                    # TODO: 2 SPEAKER RECOGNIZED ALWAYS
+                    clean_audio = noise_suppresion_SB(spk_buf)
                     speech_separation = speech_separation_SB(clean_audio)
                     n_spk = speech_separation.size(1)
                     print(f"{__file__}: Распознано {n_spk} speaker`ов")
